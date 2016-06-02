8105ddf8a0947a4451e9ff56549c189556f36029
Robert Agthe <robert@scriptshit.de>
2016-06-02 19:05:22 +0200

    commonmark parsing for threads added

--- server/app_routes/thread.js
+++ server/app_routes/thread.js
@@ -1,3 +1,5 @@
+commonmark = require("commonmark");
+
 module.exports = function(app, pdb, pdb_users, auth, PouchDB) {
 
 	// Root !
@@ -11,7 +13,11 @@ module.exports = function(app, pdb, pdb_users, auth, PouchDB) {
 
 		// query db
 		pdb.get(docId, options).then(function(response) {
+			var reader = new commonmark.Parser();
+			var writer = new commonmark.HtmlRenderer();
 			res.render("thread", {
+				m_reader: reader,
+				m_writer: writer,
 				thread: response
 			});
 		});
